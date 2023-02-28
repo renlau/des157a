af57884c1c0d3766128407279141932dba66aee8
Renee Lau <renlau@ucdavis.edu>
2023-02-28 12:20:34 -0800

    studio 2 added

---
+++ studio2/scripts.js
@@ -0,0 +1,14 @@
+(function () {
+    "use strict";
+    console.log("running js");
+
+    var cimg = document.querySelectorAll('.colorimg');
+    for (var i = 0; i < cimg.length; i++) {
+        cimg[i].addEventListener("mouseover", function () {
+            cimg.style.opacity = 1;
+        });
+        cimg[i].addEventListener("mouesout", function () {
+            cimg.style.opacity = 0;
+        });
+    }
+})();
