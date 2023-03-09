754601fb15f488d7d6a42c7ba3019e1225c6a93c
Renee Lau <renlau@ucdavis.edu>
2023-03-09 01:03:54 -0800

    studio 3 second draft

--- studio3/script.js
+++ studio3/script.js
@@ -1,124 +1,130 @@
-(function(){
+
+(function () {
     'use strict';
     console.log('running js');
 
     const startGame = document.getElementById('startgame');
-    const gameControl = document.getElementById('gamecontrol');
-    const game = document.getElementById('game');
-    const score = document.getElementById('score');
-    // const score1 = document.getElementById('score1');
-    // const score2 = document.getElementById('score2');
-    const actionArea = document.getElementById('actions');
-    var enemy;
+    const topbar = document.getElementById('topbar');
+    const gameInfo = document.getElementById('gameInfo');
+    const player1 = document.getElementById('player1');
+    const player2 = document.getElementById('player2');
+    const roll0 = document.getElementById('roll0');
+    const roll1 = document.getElementById('roll1');
+    const pass0 = document.getElementById('pass0');
+    const pass1 = document.getElementById('pass1');
+    const score0 = document.getElementById('score0');
+    const score1 = document.getElementById('score1');
+    const rules = document.getElementById('rules');
 
     const gameData = {
-        // dice: ['1die.jpg', '2die.jpg', '3die.jpg', '4die.jpg', '5die.jpg', '6die.jpg'],
         players: ['player 1', 'player 2'],
-        score: [100, 100],
-        sheild: [0,0],
+        score: [30, 30],
+        sheild: [0, 0],
         roll1: 0,
         roll2: 0,
-        rollSum: 0,
-        //which player it is (player 1 or player 2)
-        index: 0,
-        gameEnd: 0,
+        index: 0, //which player it is (player 1 or player 2)
+        enemy: 0
     }
 
-    startGame.addEventListener('click',function(){
-        document.getElementById('rules').style.display = "none";
-        document.getElementById('gamecontrol').style.display = "block";
-        gameData.index = Math.round(Math.random());
-        console.log(gameData.index);
-        gameControl.innerHTML += '<button id = "quit" class = "button3">Quit</button>';
-        document.getElementById('quit').addEventListener('click', function(){
+    startGame.addEventListener('click', function () {
+        rules.style.display = "none"; // closes the rules
+        topbar.style.display = "flex"; // show the quit button and image
+        //selects player 1 or player 2 randomly
+        gameData.index = Math.round(Math.random()); // either 0 or 1
+        player1.style.display = `flex` //CHANGE DURING FINAL WHERE ITS INPUT BASED ON THE AGENT YOU SELECT
+        player2.style.display = `flex` //CHANGE DURING FINAL WHERE ITS INPUT BASED ON THE AGENT YOU SELECT
+        switchPlayers();
+        document.getElementById('quit').addEventListener('click', function () {
             // reloads the page
-            location.reload(); 
+            location.reload();
         });
-        //either zero or one; 0 = player 1; 1 = player 2
-        console.log('set up the turn');
-        setUpTurn();
+        roll0.addEventListener("click", function () {
+            throwDice();
+        })
+        roll1.addEventListener("click", function () {
+            throwDice();
+        })
+        pass0.addEventListener('click', function () {
+            //switches players 
+            //+ 25 shield 
+            //checkgameData.sheild; if its zero add 25;
+            switchPlayers();
+        });
+        pass1.addEventListener('click', function () {
+            //switches players 
+            //+ 25 shield 
+            //checkgameData.sheild; if its zero add 25;
+            switchPlayers();
+        });
+        showCurrentScore();
     });
 
-    function setUpTurn(){
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
-        actionArea.innerHTML = '<button id="roll" class="button2">ROLL</button>';
-        document.getElementById('roll').addEventListener('click', function(){
-            console.log('roll the dice!');
-            throwDice();
-        });
-    }
-
-    function throwDice(){
-        actionArea.innerHTML = '';
+    function throwDice() {
         gameData.roll1 = Math.floor(Math.random() * 6) + 1;
         gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-        game.innerHTML = `<p>Roll the Dice for the ${gameData.players[gameData.index]}</p>`
-        // game.innerHTML += `<img src="${gameData.dice[gameData.roll1-1]}">`;
-        // game.innerHTML += `<img src="${gameData.dice[gameData.roll2-1]}">`;
-        gameData.rollSum = gameData.roll1 + gameData.roll2;
-
-        if(gameData.rollSum == 2){
-            game.innerHTML += '<p>oh snap! snake eyes! 0.0</p>';
-            gameData.score[gameData.index] = 0;
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            setTimeout(setUpTurn, 2000);
-            console.log('snake eyes!');
+        let rollSum = gameData.roll1 + gameData.roll2;
+        
+        if (rollSum == 2) {
+            gameInfo.innerHTML += `<p>You're enemy healed full health</p>`;
+            gameData.score[gameData.enemy] = 30;
+            switchPlayers();
+            showCurrentScore();
+        } else if (gameData.roll1 == 1 || gameData.roll2 == 1) {
+            switchPlayers();
+            gameInfo.innerHTML += `<p>Sorry, one of your rolls was a one. Switching to ${gameData.players[gameData.index]}</p>`;
+        } else {
+            //this is for the current player so make sure to make it the enemy; make a variable 
+            gameInfo.innerHTML = `<p>You did ${rollSum} damage</p>`;
+            gameData.score[gameData.enemy] = gameData.score[gameData.enemy] - rollSum;
+            checkWinningCondition();
             showCurrentScore();
         }
-        else if(gameData.roll1 == 1 || gameData.roll2 == 1){
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p>sorry, one of your rolls was a one. Switching to ${gameData.players[gameData.index]}</p>`;
-            setTimeout(setUpTurn, 2000);
-            console.log("wah wah your tunr is over!");
-        }
-        else{
-            //this is for the current player so make sure to make it the enemy; make a variable 
-            if (gameData.index == 0){
-                enemy = 1;
+    };
 
-            } else {
-                enemy = 0;
-            }
-            gameData.score[enemy] = gameData.score[enemy] - gameData.rollSum;
-            actionArea.innerHTML = '<button id="rollagain" class="button2">ROLL</button> <button id="pass" class="button3">PASS</button>';
-            document.getElementById('rollagain').addEventListener('click', function(){
-                setUpTurn();
-            });
-            document.getElementById('pass').addEventListener('click', function(){
-                //switches players 
-                //+ 25 shield 
-                //checkgameData.sheild; if its zero add 25;
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            });
-            checkWinningCondition();
-            console.log("game continues");
+    function switchPlayers () {
+        if (gameData.index === 0) { // player1's turn is over
+            gameData.index = 1; //changes over to the other player
+            gameData.enemy = 0; //changes over to the enemy
+            player1.classList.add("disabled"); //adds the opacity to player 1
+            roll0.setAttribute("disabled", "");
+            roll0.classList.remove("button2h");
+            pass0.setAttribute("disabled", "");
+            pass0.classList.remove("button3h");
+            player2.classList.remove("disabled"); //removes the opacity to player 2
+            roll1.removeAttribute("disabled");
+            roll1.classList.add("button2h");
+            pass1.removeAttribute("disabled");
+            pass1.classList.add("button3h");
+        } else { // player2's turn is over
+            gameData.index = 0; //changes player to player 1 
+            gameData.enemy = 1; //changes enemy to player 2
+            player1.classList.remove("disabled"); //removes the opacity to player 1
+            roll0.removeAttribute("disabled");
+            roll0.classList.add("button2h")
+            pass0.removeAttribute("disabled");
+            pass0.classList.add("button3h")
+            player2.classList.add("disabled");//adds the opacity to player 2
+            roll1.setAttribute("disabled", "");
+            roll1.classList.remove("button2h");
+            pass1.setAttribute("disabled", "");
+            pass1.classList.remove("button3h");
         }
-
-    };  
+    }
 
     //REVISIT HERE
 
-    function checkWinningCondition(){
+    function checkWinningCondition() {
         //if current score of the current player is greater than the end game score then game ends
-        if(gameData.score[gameData.index] < gameData.gameEnd){
-            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points</h2>`
-
-            actionArea.innerHTML = '';
+        if (gameData.score[gameData.enemy] <= 0) {
+            gameInfo.innerHTML = `<h1>${gameData.players[gameData.index]} wins</h1>`
             document.getElementById('quit').innerHTML = "Start a New Game";
         }
-
-        else {
-            //show current score...
-            showCurrentScore();
-        }
-        function showCurrentScore(){
-            document.getElementById('score').style.display = "block";
-            // score1.innerHTML += `<h1>${gameData.score[0]}</h1>`;
-            // score2.innerHTML += `<h1>${gameData.score[1]}</h1>`;
-            score.innerHTML = `<p>the score is currently <strong>${gameData.players[0]} ${gameData.score[0]}</strong> and <strong>${gameData.players[1]} ${gameData.score[1]}</strong></p>`;
-        }
     }
-
-
+    
+    function showCurrentScore() {
+        score0.style.display = "block";
+        score1.style.display = "block";
+        score0.innerHTML = `<h1>${gameData.score[0]}</h1>`;
+        score1.innerHTML = `<h1>${gameData.score[1]}</h1>`;
+    }
 })();
